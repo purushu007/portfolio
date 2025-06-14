e2e81728370ed9480ec41e55cdb5ab6bb264266a
Purushothama Reddy M <purushothama.reddy.m@oracle.com>
2025-06-15 01:38:21 +0530

    docs: update about description with consistent formatting

--- constants/data.ts
+++ constants/data.ts
@@ -1,15 +1,17 @@
 export const data = {
-  name: "M Purushothama Reddy",
+  name: "Purushothama Reddy M",
+  designation: "Full Stack Developer",
   avatarUrl: "/images/profile-pic.JPG",
   email: "purushu574@gmail.com",
   phone: "+91 9985925280",
   address: "Madanapalle, Andhra Pradesh, India",
   about: {
-    description: "I'm a software developer 👨🏽‍💻 loves building products and web applications that impact millions of lives 🌏. A Front-End Developer with 7 years of experience in blending the art of design 🎨 with skill of programming 🧑‍🎨 to deliver an engaging user experience. Excited about Product 📦, Design 🎨 and aesthetics ✨."
+    description:
+      "I'm a Full Stack Developer 👨🏽‍💻 with 7 years of experience in building impactful web applications 🌏. I specialize in creating engaging user experiences by combining design aesthetics 🎨 with technical expertise 🧑‍🎨. Passionate about product development 📦, UI/UX design 🎨, and creating beautiful digital experiences ✨.",
   },
   resumeUrl: "https://docs.google.com/document/d/1Qc4UOca-txrccPew-H_XjqFlqvl7nq8o/edit?usp=sharing&ouid=100718242050812473535&rtpof=true&sd=true",
   socialLinks: {
     linkedin: "https://www.linkedin.com/in/m-purushothama-reddy/",
     github: "https://github.com/purushu007"
-  }
+  },
 };
